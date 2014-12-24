9960a5691a4fc495a1e11293c2f14293ac82f9a7
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2014-12-24 15:23:07 +0800

    extract crudie spec

--- lib/crudie/spec.rb
+++ lib/crudie/spec.rb
@@ -1,16 +1,40 @@
 module Crudie
   module Spec
-    def test_post resource
+
+    def test_index resource
+      resource = :project
+      resources = :projects
+      context = :user
+
+      describe 'GET :index' do
+        let(:crudie_context) { double(:crudie_context, :find => crudie_instance) }
+        let(:crudie_instance) { double(:crudie_instance, :valid? => true) }
+        before :each do
+          allow(controller).to receive(:crudie_context).and_return(crudie_context)
+        end
+        let(:params) do
+          {
+            :user_id => 123
+          }
+        end
+        it "returns projects of user" do
+          get :index, params
+          expect(controller.instance_variable_get("@#{resources}")).to be(crudie_context)
+          expect(response.status).to eq 200
+        end
+      end
+      
+    end
+
+    def test_create resource
       resource = :project
       resources = :projects
       context = :user
 
 
       describe "POST :create" do
-        let(:crudie_context) { double(:curdie_context, :find => crudie_instance) }
-        let(:crudie_instance) { double(:curdie_instance, :valid? => true) }
-
-
+        let(:crudie_context) { double(:crudie_context, :find => crudie_instance) }
+        let(:crudie_instance) { double(:crudie_instance, :valid? => true) }
         before :each do
           allow(controller).to receive(:crudie_context).and_return(crudie_context)
         end
@@ -46,5 +70,105 @@ module Crudie
         end
       end
     end
+
+
+    def test_show resource
+      resource = :project
+      resources = :projects
+      context = :user
+
+      describe "GET :show" do
+        let(:crudie_context) { double(:crudie_context, :find => crudie_instance) }
+        let(:crudie_instance) { double(:crudie_instance, :valid? => true) }
+        before :each do
+          allow(controller).to receive(:crudie_context).and_return(crudie_context)
+        end
+        let(:params) do
+          {
+            :id => 111,
+            :user_id => 123
+          }
+        end
+        before :each do
+          allow(crudie_context).to receive(:find).and_return(crudie_instance)
+        end
+
+        it "shows the resource instance by id" do
+          get :show, params
+          expect(controller.instance_variable_get("@#{resource}")).to be crudie_instance 
+        end
+      end
+      
+    end
+
+    def test_update resource
+      resource = :project
+      resources = :projects
+      context = :user
+
+      describe "PUT :update" do
+        let(:crudie_context) { double(:crudie_context, :find => crudie_instance) }
+        let(:crudie_instance) { double(:crudie_instance, :valid? => true) }
+        before :each do
+          allow(controller).to receive(:crudie_context).and_return(crudie_context)
+        end
+        let(:params) do
+          {
+            :id => 111,
+            :user_id => 123,
+            :project => {
+              :name => 'new name'
+            }
+          }
+        end
+        before :each do
+          allow(crudie_context).to receive(:find).and_return(crudie_instance)
+        end
+
+        it "updates instance" do
+          expect(crudie_instance).to receive(:update_attributes).with(params[resource])
+                                                                .and_return(true)
+          put :update, params
+          expect(controller.instance_variable_get("@#{resource}")).to be crudie_instance
+        end
+
+        it "render 409 if updating failed" do
+          expect(crudie_instance).to receive(:update_attributes)
+                                       .with(params[resource]).and_return(false)
+          expect(crudie_instance).to receive(:errors).and_return(double(:messages => 'the messages'))
+
+          put :update, params
+          expect(response.status).to eq 409
+        end
+        
+      end
+
+    end
+
+    def test_destroy resource
+      describe "DELETE :destroy" do
+        let(:crudie_context) { double(:crudie_context, :find => crudie_instance) }
+        let(:crudie_instance) { double(:crudie_instance, :valid? => true) }
+        before :each do
+          allow(controller).to receive(:crudie_context).and_return(crudie_context)
+        end
+        let(:params) do
+          {
+            :id => 111,
+            :user_id => 123
+          }
+        end
+        before :each do
+          allow(crudie_context).to receive(:find).and_return(crudie_instance)
+        end
+        
+        it "destroy the instance" do
+          expect(crudie_instance).to receive(:destroy)
+          delete :destroy, params
+        end
+      end
+      
+    end
+
   end
 end
--- spec/test_app/spec/controllers/crudie_spec.rb
+++ spec/test_app/spec/controllers/crudie_spec.rb
@@ -3,89 +3,9 @@ require 'crudie/spec'
 
 RSpec.describe ProjectsController, :type => :controller do
   extend Crudie::Spec
-  test_post(:project)
-
-  let(:user) { double(:user, :projects => projects) }
-  let(:projects) { double(:projects) }
-  let(:project) { double(:project, :valid? => true) }
-  before :each do
-    allow(User).to receive(:find).and_return(user)
-  end
-
-  describe 'GET :index' do
-    let(:params) do
-      {
-        :user_id => 123
-      }
-    end
-    it "returns projects of user" do
-      get :index, params
-      expect(controller.instance_variable_get(:@projects)).to be(projects)
-      expect(response.status).to eq 200
-    end
-  end
-
-  describe "GET :show" do
-    let(:params) do
-      {
-        :id => 111,
-        :user_id => 123
-      }
-    end
-    before :each do
-      allow(projects).to receive(:find).and_return(project)
-    end
-
-    it "shows the resource instance by id" do
-      get :show, params
-      expect(controller.instance_variable_get(:@project)).to be project
-    end
-  end
-
-  describe "PUT :update" do
-    let(:params) do
-      {
-        :id => 111,
-        :user_id => 123,
-        :project => {
-          :name => 'new name'
-        }
-      }
-    end
-    before :each do
-      allow(projects).to receive(:find).and_return(project)
-    end
-
-    it "updates instance" do
-      expect(project).to receive(:update_attributes).with(params[:project]).and_return(true)
-      put :update, params
-      expect(controller.instance_variable_get(:@project)).to be project
-    end
-
-    it "render 409 if updating failed" do
-      expect(project).to receive(:update_attributes).with(params[:project]).and_return(false)
-      expect(project).to receive(:errors).and_return(double(:messages => 'the messages'))
-
-      put :update, params
-      expect(response.status).to eq 409
-    end
-    
-  end
-
-  describe "DELETE :destroy" do
-    let(:params) do
-      {
-        :id => 111,
-        :user_id => 123
-      }
-    end
-    before :each do
-      allow(projects).to receive(:find).and_return(project)
-    end
-    
-    it "destroy the instance" do
-      expect(project).to receive(:destroy)
-      delete :destroy, params
-    end
-  end
+  test_create(:project)
+  test_index(:project)
+  test_show(:project)
+  test_update(:project)
+  test_destroy(:project)
 end
